20c3d26934b7aa8d765baf94b38bb249ef3e65b7
ranamomin <ranamomin@gmail.com>
2020-11-20 19:02:47 -0800

    pickle.load added to load the model, along with some instructions

--- webApp/backend/app/routes.py
+++ webApp/backend/app/routes.py
@@ -1,6 +1,15 @@
 from app import app
 import random
+import pickle
 
+'''
+We need to open the model.pkl file using this:
+model = pickle.load(open("model.pkl",'rb'))
+
+make sure that model pickle file is created in modeling.ipynb 
+if not then use this to create one in modeling.ipynb file:
+    pickle.dump(name_of_model, open('model.pkl','wb'))
+'''
 
 @app.route('/predict')
 def predict():
