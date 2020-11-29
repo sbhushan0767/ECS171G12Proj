1b04f0fd34698c08361ca6b0c235d0893bb41a9e
kenkong03014 <48498246+kenkong03014@users.noreply.github.com>
2020-11-28 23:08:03 -0800

    delete unnccessary code

--- webApp/backend/app/models/linearModel.py
+++ webApp/backend/app/models/linearModel.py
@@ -14,22 +14,6 @@ from imblearn.over_sampling import RandomOverSampler
 # Read in processed data
 df = pd.read_csv('../../../../datasets/credit.csv')
 
-le = LabelEncoder()
-df[["Credit Score Range"]] = df[["Credit Score Range"]].apply(le.fit_transform)
-
-to_drop = ["Credit Score", "Credit Score Range"]
-X = df.drop(to_drop, axis=1)
-labels = df["Credit Score Range"]
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, labels, test_size=0.25, random_state=1)
-
-X_train = X_train.to_numpy()
-y_train = y_train.to_numpy()
-
-ros = RandomOverSampler(random_state=0)
-X_resampled, y_resampled = ros.fit_resample(X_train, y_train)
-
 X = df.drop(columns=['Loan ID', 'Customer ID', 'Credit Score Range',
                      'Credit Score'])
 
